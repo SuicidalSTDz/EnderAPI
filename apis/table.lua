2b2e3daa0445e60833211a0be334ffd9025bd962
Tyler McFarland <suicidalstdz@gmail.com>
2014-03-22 10:45:13 -0400

    Create table.lua

---
+++ apis/table.lua
@@ -0,0 +1,24 @@
+local function assert(bBool, sMessage, nLevel)
+	local nLevel = nLevel or -1
+	if type(sMessage) ~= "string" then
+		error("String expected, got " .. type( sMessage ), 2)
+	elseif type(nLevel) ~= "number" then
+		error("Number expected, got " .. type( nLevel ), 2)
+	end
+	
+	if not bBool then
+		error( sMessage, iLevel + 1 )
+	end
+	return bBool
+end
+
+function table.sortLToG( tbl )
+  assert( type( tbl ) == "table", "Table expected, got " .. type( tbl ), 2 )
+  table.sort( tbl, function( a, b ) return a < b end )
+  return tbl
+end
+
+function table.sortGToL( tbl )
+  table.sort( tbl, function( a, b ) return a > b end )
+  return tbl
+end
