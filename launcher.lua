747e3476bb1614698c37867b4fd5328f1261955a
Tyler McFarland <suicidalstdz@gmail.com>
2014-03-22 13:54:34 -0400

    Update launcher.lua
    Fixed. Must not have seen the update.. :P

--- launcher.lua
+++ launcher.lua
@@ -1,266 +1,337 @@
---# Check if the user has the http module enabled
-if not http then
-  error("HTTP is required to run this!", 0)
+local folder = "/.EnderAPI/"
+-- Make it backwards compatible, remove it after a while though
+if fs.exists( "/.EnderAPI/master/apis" ) and fs.isDir( "/.EnderAPI/master/apis/" ) then
+  for _, name in ipairs( fs.list( "/.EnderAPI/master/apis/" ) ) do
+    fs.move( "/.EnderAPI/master/apis/" .. name, "/.EnderAPI/" .. name )
+  end
+  fs.delete( "/.EnderAPI/master/" )
 end
 
---# USER VARIABLE(S)
-local autoUpdate = true
-local askUser = true
-
---# Other variables, no touchy!
 local tArgs = { ... }
-local tUpdate = {}
-local tDownloaded = {}
-local tbl = {}
-local w, h = term.getSize()
+local showGUI = ( term.isColor and term.isColor() )
+local updateAPI = true
+local updateLauncher = true
 
---# Allow temporary update disabling and temporary override of updating
-if tArgs[ 1 ] == "false" then
-  autoUpdate = false
-end
-if tArgs[ 2 ] == "false" then
-  askUser = false
-end
-
---# Files to grab when updating/installing
-local tFiles = {
-  "/EnderAPI/master/apis/number.lua",
-  "/EnderAPI/master/apis/pastebin.lua",
-  "/EnderAPI/master/apis/text.lua",
-  "/EnderAPI/master/apis/messageBox.lua",
-  "/EnderAPI/master/apis/http.lua",
-  "/EnderAPI/master/apis/term.lua",
-  "/EnderAPI/master/apis/string.lua",
-  "/EnderAPI/master/apis/fs.lua",
-  "/EnderAPI/master/apis/table.lua",
-  "/EnderAPI/master/apis/enderAPI.lua",
-  "/EnderAPI/master/version.lua"
-}
-
---# Create directories if they are not present
-if not fs.exists( "/.EnderAPI" ) then
-  fs.makeDir( "/.EnderAPI" )
-end
-if not fs.exists( "/.EnderAPI/master" ) then
-  fs.makeDir( "/.EnderAPI/master" )
-end
-if not fs.exists( "/.EnderAPI/master/apis" ) then
-  fs.makeDir( "/.EnderAPI/master/apis" )
-end
-if not fs.exists( "/.EnderAPI/master/temp" ) then
-  fs.makeDir( "/.EnderAPI/master/temp" )
-end
-if not fs.exists( "/.EnderAPI/master/help" ) then
-  fs.makeDir( "/.EnderAPI/master/help" )
-end
-
-local function update( tLines, sHeader, sFooter )
-  local nOffset = 0
-  term.setTextColour( colours.black )
-  term.setTextColour( colours.white )
-  term.clear()
-  paintutils.drawLine( 1, 1, w, 1, colours.purple )
-  paintutils.drawLine( 1, h, w, h, colours.purple )
-  term.setTextColour( colours.white )
-  term.setCursorPos( ( w - #sHeader ) / 2 + 1, 1 )
-  term.write( sHeader )
-  term.setCursorPos( 2, h )
-  term.write( sFooter )
-  term.setCursorPos( w - 6, h )
-  term.write( "[Done]" )
-  term.setBackgroundColour( colours.black )
-  
-  local function redraw()
-    for i = 2, h - 1 do
-      if tLines[ ( i - 1 ) + nOffset ] then
-        term.setCursorPos( 1, i )
-        if tbl[ ( i - 1 ) + nOffset ].update then
-          term.setTextColour( colours.lime )
-        else
-          term.setTextColour( colours.red )
-        end
-        term.write( tLines[ ( i - 1 ) + nOffset ] )
-      end
+for i, v in ipairs( tArgs ) do
+  local a = v:lower()
+  if a == "nogui" then 
+    showGUI = false 
+  elseif a == "-n" and tArgs[ i + 1 ] then 
+    if tArgs[ i + 1 ] == "api" then
+      updateAPI = false
+    elseif tArgs[ i + 1 ] == "launcher" then
+      updateLauncher = false
     end
   end
-  
-  for i = nOffset + 1, ( h + nOffset ) - 2 do
-    if tLines[ i ] then
-      table.insert( tbl, {
-        endX = #tLines[ i ],
-        y = i + 1 - nOffset,
-        data = tLines[ i ],
-        update = true
-      })
-    end
+end
+
+if updateLauncher then
+  if not http then
+    error( "HTTP is required to search for updates!", 0 )
   end
-  
-  redraw()
-  while true do
-    local oldy
-    local sEvent, param, nx, ny = os.pullEvent()
+
+  print( "Checking for launcher update.." )
+  local httpHandle = http.get( "https://raw.github.com/SuicidalSTDz/EnderAPI/master/launcher.lua" )
+  if httpHandle then
+    local httpContent = httpHandle.readAll()
+    httpHandle.close()
     
-    --# 'mouse_scroll'
-    if sEvent == "mouse_scroll" then
-      if param == 1 and nOffset < math.max( #tLines - ( h - 2 ), 0 ) then
-        nOffset = nOffset + 1
-      elseif param == -1 and nOffset > 0 then
-        nOffset = nOffset - 1
-      end
-      redraw()
-    
-    --# 'mouse_click' and 'mouse_drag'
-    elseif (sEvent == "mouse_click" or sEvent == "mouse_drag") and param == 1 then
-      if oldy and sEvent == "mouse_drag" and oldy == ny then
-        -- Do nothing, prevent buttons going crazy when dragging along X axis
-      else
-        oldy = ny
-        if ( nx >= w - 6 and nx <= w and ny == h ) then
-          break
-        end
-        for _, tData in pairs( tbl ) do
-          if ( nx >= 1 and nx <= tData.endX and ny == tData.y ) then
-            tData.update = not tData.update
+    if httpContent ~= "" then
+      local sFile = shell.getRunningProgram()
+     
+      local fileHandle = fs.open( sFile, 'r' )
+      local fileContent = fileHandle.readAll()
+      fileHandle.close()
+      
+      if fileContent ~= httpContent then
+        local updateFile = true
+        local oldX, oldY
+        
+        if showGUI then
+          local w, h = term.getSize()
+          
+          term.setBackgroundColour( colours.black )
+          term.clear()
+          
+           -- Draw the outline, without paintutils :P
+          term.setBackgroundColour( colours.purple )
+          local beginBoxW = ( w - 25 ) / 2
+          local beginBoxH = math.floor( h / 2 ) - 2
+          for i = 0, 3 do
+            term.setCursorPos( beginBoxW - 1, beginBoxH + i )
+            term.write( ' ' )
+            term.setCursorPos( beginBoxW + 26, beginBoxH + i )
+            term.write( ' ' )
+          end
+          
+          for i = 0, 25 do
+            term.setCursorPos( beginBoxW + i, beginBoxH )
+            term.write( ' ' )
+            term.setCursorPos( beginBoxW + i, beginBoxH + 3 )
+            term.write( ' ' )
+          end
+          
+          -- Write the text
+          term.setBackgroundColour( colours.black )
+          term.setTextColour( colours.white )
+          term.setCursorPos( beginBoxW, math.floor( h / 2 ) - 1 )
+          term.write( "An update has been found" )
+          
+          oldX, oldY = term.getCursorPos()
+          term.setCursorPos( oldX - 24, oldY + 1 )
+          term.write( "for the launcher, update?" )
+          
+          term.setBackgroundColour( colours.purple )
+          oldX, oldY = term.getCursorPos()
+          term.setCursorPos( oldX - 24, oldY + 1 )
+          term.write( "[ Yes ]" )
+          
+          term.setCursorPos( oldX - 7, oldY + 1 )
+          term.write( "[ No ]" )
+          
+          term.setBackgroundColour( colours.black )
+          
+          while true do
+            local ev = { os.pullEvent() }
+            if ev[ 1 ] == "mouse_click" then
+              if ev[ 3 ] >= oldX - 24 and ev[ 3 ] <= oldX - 17 and ev[ 4 ] == oldY + 1 then
+                term.clear()
+                term.setCursorPos( 1, 1 )
+                break
+              elseif ev[ 3 ] >= oldX - 7 and ev[ 3 ] <= oldX - 1 and ev[ 4 ] == oldY + 1 then
+                updateFile = false
+                term.clear()
+                term.setCursorPos( 1, 1 )
+                break
+              end
+            end
           end
         end
-        redraw()
+        
+        if updateFile then
+          fileHandle = fs.open( sFile, 'w' )
+          fileHandle.write( httpContent )
+          fileHandle.close()
+          
+          local arguments = {}
+          if not gui then
+            table.insert( arguments, "nogui" )
+          end
+          if not updateAPI then
+            table.insert( arguments, "-n" )
+            table.insert( arguments, "api" )
+          end
+          table.insert( arguments, "-n" )
+          table.insert( arguments, "launcher" )
+          
+          shell.run( shell.getRunningProgram(), unpack( arguments ) )
+          return
+        end
       end
     end
   end
 end
 
-local function loadAPIs()
-  --# Store the old os.loadAPI function and replace it with a new one. We don't want any terminal output
-  local tAPIsLoading = {}
-  
-  local function loadAPI( _sPath )
-    local sName = fs.getName( _sPath )
-    if tAPIsLoading[sName] == true then
-      return false
-    end
-    tAPIsLoading[sName] = true
+if updateAPI then
+  if not http then
+    error( "HTTP is required to search for updates!", 0 )
+  end
+
+  local baseURL = "https://raw.github.com/SuicidalSTDz/EnderAPI/master/apis/"
+  local folderExisted = true
+  local tFiles = { 
+    [ "fs.lua" ] = {},
+    [ "http.lua" ] = {},
+    --[ "messageBox.lua" ] = {},
+    [ "number.lua" ] = {},
+    [ "pastebin.lua" ] = {},
+    [ "string.lua" ] = {},
+    [ "term.lua" ] = {},
+    [ "text.lua" ] = {},
+    [ "table.lua" ] = {}
+  }
+
+  if not fs.exists( folder ) or not fs.isDir( folder ) then
+    fs.makeDir( folder )
+    folderExisted = false
+  end
+
+  -- Let the user know that (s)he has to wait a minute
+  print( "Downloading files, this can take a while.." )
+
+  -- Download & check files
+  for luaFile, tbl in pairs( tFiles ) do
+    tbl.fileName = string.sub( luaFile, 1, luaFile:len() - 4 )
+
+    local httpHandle = http.get( baseURL .. luaFile )
+    if httpHandle then
+      tbl.content = httpHandle.readAll()
+      httpHandle.close()
       
-    local tEnv = {}
-    setmetatable( tEnv, { __index = _G } )
-    local fnAPI, err = loadfile( _sPath )
-    if fnAPI then
-      setfenv( fnAPI, tEnv )
-      fnAPI()
-    else
-      tAPIsLoading[sName] = nil
-      return false
-    end
-    
-    local tAPI = {}
-    for k,v in pairs( tEnv ) do
-      tAPI[k] =  v
-    end
-    
-    _G[sName] = tAPI  
-    tAPIsLoading[sName] = nil
-    return true
-  end
-  
-  loadAPI( "/.EnderAPI/master/apis/text" )
-  loadAPI( "/.EnderAPI/master/apis/number" )
-  loadAPI( "/.EnderAPI/master/apis/pastebin" )
-  loadAPI( "/.EnderAPI/master/apis/messageBox" )
-  loadAPI( "/.EnderAPI/master/apis/enderAPI" )
-  shell.run( "/.EnderAPI/master/apis/http" )
-  shell.run( "/.EnderAPI/master/apis/term" )
-  shell.run( "/.EnderAPI/master/apis/string" )
-  shell.run( "/.EnderAPI/master/apis/fs" )
-  shell.run( "/.EnderAPI/master/apis/table" )
-end
-
---# Main Update Script
-for i = 1, #tFiles do
-  --# Grab tFiles[ i ] from Github and store its contents in sResponse
-  local sResponse, sData
-  local response = http.get( "https://raw.github.com/SuicidalSTDz" .. tFiles[ i ] )
-  if response then
-    sResponse = response.readAll()
-    response.close()
-  else
-    sResponse = ""
-  end
-  
-  --# Open sFile and store its contents in sData
-  local sFile = (tFiles[ i ]:sub( 1, 1 ) .. "." .. tFiles[ i ]:sub( 2, #tFiles[ i ] )):gsub("%.lua", "")
-  if not fs.exists( sFile ) then
-    local handle = io.open( sFile, "w" )
-    handle:write( "This file was improperly downloaded, please try again later" )
-    handle:close()
-  else
-    local handle = fs.open( sFile, "r" )
-    sData = handle.readAll()
-    handle.close()
-  end
-    
-  --# Check sResponse against sData
-  if ( sResponse ~= "" ) and ( sData ~= sResponse ) then
-    tDownloaded[ sFile ] = sResponse
-    table.insert( tUpdate, sFile )
-  end
-end
-  
---# If there are any updates, then update
-if #tUpdate > 0 then
-  if askUser then
-    update( tUpdate, "EnderAPI would like to update these files", "Click on a file to set its update value" )
-    --# Parse through tbl and update
-    for k, v in pairs( tbl ) do
-      if v.update then
-        local handle = io.open( v.data, "w" )
-        handle:write( tDownloaded[ v.data ] )
-        handle:close()
-      end
-    end
-  else
-    --# Don't ask the user to update, do it behind their back!
-    for k, v in pairs( tDownloaded ) do
-      local handle = io.open( k, "w" )
-      handle:write( v )
-      handle:close()
-    end
-  end
-end
-
---# Update the launcher, if need be
-if autoUpdate then
-  tbl = {}
-  local sResponse, sData
-  local sRunning = shell.getRunningProgram()
-  local response = http.get( "https://raw.github.com/SuicidalSTDz/EnderAPI/master/launcher" )
-  if response then
-    sResponse = response.readAll()
-    response.close()
-    
-    local handle = fs.open( sRunning, "r" )
-    local sData = handle.readAll()
-    handle.close()
-    
-    if sResponse ~= "" and ( sData ~= sResponse ) then
-      if askUser then
-        update( { sRunning }, "EnderAPI would like to update these files", "Click on a file to set its update value" )
-        if tbl[ 1 ].update then
-          local handle = io.open( sRunning, "w" )
-          handle:write( sResponse )
-          handle:close()
+      if folderExisted then
+        if fs.exists( folder .. tbl.fileName ) then
+          local fileHandle = fs.open( folder .. tbl.fileName, "r" )
+          local content = fileHandle.readAll()
+          fileHandle.close()
+          
+          tbl.update = ( content ~= tbl.content and tbl.content ~= "" )
+        else
+          tbl.update = true
         end
       else
-        local handle = io.open( sRunning, "w" )
-        handle:write( sResponse )
-        handle:close()
+        tbl.update = true
       end
+    else
+      tbl.update = false
+    end
+  end
+
+  if showGUI then  
+    local w, h = term.getSize()
+    local availableFiles = {}
+   
+   for k, v in pairs( tFiles ) do
+      if v.update then
+        table.insert( availableFiles, {
+          name = v.fileName,
+          colour = 'g'
+        } )
+      end
+    end
+    
+    term.setBackgroundColour( colours.black )
+    term.clear()
+    
+    term.setBackgroundColour( colours.purple )
+    for i = 1, w do
+      term.setCursorPos( i, 1 )
+      term.write( " " )
+      term.setCursorPos( i, h )
+      term.write( " " )
+    end
+    
+    term.setTextColour( colours.white )
+    term.setCursorPos( 2, 1 )
+    term.write( "EnderAPI would like to update these files" )
+    
+    term.setCursorPos( 2, h )
+    term.write( "Click on a file to set its update value" )
+    
+    term.setCursorPos( w - 9, h )
+    term.write( "[ Done ]" )
+    
+    term.setTextColour( colours.green )
+    
+    local offset = 1
+    local function redraw()
+      term.setBackgroundColour( colours.black )
+      
+      local lastColour = 'g'
+      for i = offset, #availableFiles do
+        term.setCursorPos( 2, i - offset + 2 )
+        
+        -- Reduces server -> client packets
+       -- if availableFiles[ i ].colour ~= lastColour then
+          lastColour = availableFiles[ i ].colour
+          if lastColour == 'g' then
+            term.setTextColour( colours.green )
+          elseif lastColour == 'r' then
+            term.setTextColour( colours.red )
+          end
+       -- end
+        term.write( availableFiles[ i ].name )
+      end
+    end
+    
+    redraw()
+    while true do
+      local event = { os.pullEvent() }
+      if event[ 1 ] == "mouse_click" then
+        if event[ 3 ] >= w - 9 and event[ 3 ] <= w - 1 and event[ 4 ] == h then
+          for k, v in pairs( availableFiles ) do
+            if v.colour == 'r' then
+              tFiles[ v.name .. ".lua" ].update = false
+            end
+          end
+          
+          term.setBackgroundColour( colours.black )
+          term.clear()
+          term.setCursorPos( 1, 1 )
+          break
+        elseif event[ 4 ] ~= h and event[ 4 ] ~= 1 then
+          local index = offset + event[ 4 ] - 2
+          if availableFiles[ index ] then
+            if availableFiles[ index ].colour == 'g' then
+              availableFiles[ index ].colour = 'r'
+            else
+              availableFiles[ index ].colour = 'g'
+            end
+            redraw()
+          end
+        end
+      elseif event[ 1 ] == "mouse_scroll" then
+        -- Add scrolling
+      end
+    end
+  end
+
+  -- Update files
+  for _, tFile in pairs( tFiles ) do
+    if tFile.update then
+      local fileHandle = fs.open( folder .. tFile.fileName, "w" )
+      fileHandle.write( tFile.content )
+      fileHandle.close()
     end
   end
 end
 
-loadAPIs()
-if term.enderAPI then
-  term.reset()
+if fs.exists( folder ) then
+  local function loadAPI( sPath )
+    if fs.exists( sPath ) then
+      local fileHandle = fs.open( sPath, "r" )
+      local content = fileHandle.readAll()
+      fileHandle.close()
+      
+      local func, err = loadstring( content, fs.getName( sPath ) )
+      if func then
+        local tEnv = setmetatable( {}, { __index = _G } )
+        setfenv( func, tEnv )
+        
+        local ok, err = pcall( func )
+        if not ok then
+          return false
+        end
+        
+        local tAPI = {}
+        for k, v in pairs( tEnv ) do
+          tAPI[ k ] = v
+        end
+        
+        if not tAPI.isExtension then
+          _G[ fs.getName( sPath ) ] = setmetatable( {}, { 
+            __index = function( t, k )
+              if tAPI[ k ] then
+                return tAPI[ k ]
+              end
+              return
+            end,
+            __newindex = function( t, k )
+              -- Dont set anything
+            end
+          } )
+          return true
+        end
+        
+        tAPI.isExtension = nil
+        return true
+      end
+    end
+    return false
+  end
+
+  for _, file in pairs( fs.list( folder ) ) do
+    loadAPI( file )
+  end
 else
-  term.clear()
-  term.setCursorPos(1,1)
+  error( "Update first!", 0 ) 
 end
