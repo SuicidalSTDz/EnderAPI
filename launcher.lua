3a75bb649920d532e651b26a4ee37add568d6f13
Tyler McFarland <suicidalstdz@gmail.com>
2014-03-27 21:59:52 -0400

    Fixes a nasty bug
    Fixes bug with loading native APIs instead of running them

--- launcher.lua
+++ launcher.lua
@@ -334,7 +334,7 @@ if fs.exists( folder ) then
         for k, v in pairs( tEnv ) do
           tAPI[ k ] = v
         end
-        
+                
         if not tAPI.isExtension then
           _G[ fs.getName( sPath ) ] = setmetatable( {}, { 
             __index = function( t, k )
@@ -348,6 +348,8 @@ if fs.exists( folder ) then
             end
           } )
           return true
+        else
+          shell.run( sPath )
         end
         
         tAPI.isExtension = nil
