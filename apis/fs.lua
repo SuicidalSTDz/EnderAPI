a696adbdf8d285ac9d76cd81b715cfc6ea74ea8e
Tyler McFarland <suicidalstdz@gmail.com>
2014-12-22 10:43:36 -0500

    Update fs.lua

--- apis/fs.lua
+++ apis/fs.lua
@@ -13,16 +13,16 @@ end
 isExtension = true
 
 function fs.save( sPath, sData )
-  assert( type( sPath ) == "string", "String expected, got " .. type( sPath ), 2 )
-  assert( type( sData ) == "string", "String expected, got " .. type( sData ), 2 )
+  assert( type( sPath ) == "string", "string expected, got " .. type( sPath ), 2 )
+  assert( type( sData ) == "string", "string expected, got " .. type( sData ), 2 )
   local f = fs.open( sPath, "w" )
   f.write( sData )
   f.close()
 end
 
 function fs.append( sPath, sData )
-  assert( type( sPath ) == "string", "String expected, got " .. type( sPath ), 2 )
-  assert( type( sData ) == "string", "String expected, got " .. type( sData ), 2 )
+  assert( type( sPath ) == "string", "string expected, got " .. type( sPath ), 2 )
+  assert( type( sData ) == "string", "string expected, got " .. type( sData ), 2 )
   local f
   if not fs.exists(sPath) then
     f = fs.open( sPath, "w" )
